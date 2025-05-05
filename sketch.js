29f810fcd4b7d51cf24a9f5a7cf94e97fb0a7bae
stella0808 <chichihsu95@gmail.com>
2025-05-05 10:27:44 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function draw() {
   if (hands.length > 0) {
     for (let hand of hands) {
       if (hand.confidence > 0.1) {
-        // Loop through keypoints and draw circles
+        // Draw keypoints and connect index 0 to 4 with lines
         for (let i = 0; i < hand.keypoints.length; i++) {
           let keypoint = hand.keypoints[i];
 
@@ -47,6 +47,40 @@ function draw() {
 
           noStroke();
           circle(keypoint.x, keypoint.y, 16);
+        }
+
+        // Draw lines connecting keypoints 0 to 4
+        strokeWeight(2);
+        if (hand.handedness == "Left") {
+          stroke(255, 0, 255); // Left hand color
+        } else {
+          stroke(255, 255, 0); // Right hand color
+        }
+
+        for (let j = 0; j < 4; j++) {
+          let start = hand.keypoints[j];
+          let end = hand.keypoints[j + 1];
+          line(start.x, start.y, end.x, end.y);
+        }
+        for (let j = 5; j < 8; j++) {
+          let start = hand.keypoints[j];
+          let end = hand.keypoints[j + 1];
+          line(start.x, start.y, end.x, end.y);
+        }
+        for (let j = 9; j < 12; j++) {
+          let start = hand.keypoints[j];
+          let end = hand.keypoints[j + 1];
+          line(start.x, start.y, end.x, end.y);
+        }
+        for (let j = 13; j < 16; j++) {
+          let start = hand.keypoints[j];
+          let end = hand.keypoints[j + 1];
+          line(start.x, start.y, end.x, end.y);
+        }
+        for (let j = 17; j < 20; j++) {
+          let start = hand.keypoints[j];
+          let end = hand.keypoints[j + 1];
+          line(start.x, start.y, end.x, end.y);
         }
       }
     }
